34ba262e22c70c0c571a99c4a9ba84cc953a03e2
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-11-21 18:08:03 -0500

    create db/create.sql and seeds

---
+++ src/db/schema/create.sql
@@ -0,0 +1,81 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS appointments CASCADE;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  created_at DEFAULT CURRENT_TIMESTAMP,
+  updated_at ON UPDATE CURRENT_TIMESTAMP
+);
+
+
+
+CREATE TABLE appointments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  mentor_id INTEGER REFERENCES mentors(id) ON DELETE CASCADE,
+  appointment_time DEFAULT CURRENT_TIMESTAMP,
+  created_at DEFAULT CURRENT_TIMESTAMP,
+  updated_at ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE mentors (
+  id SERIAL PRIMARY KEY NOT NULL,
+  job_id INTEGER REFERENCES jobs(id) ON DELETE CASCADE,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  available_times VARCHAR(255),
+  booked_times VARCHAR(255),
+  created_at DEFAULT CURRENT_TIMESTAMP,
+  updated_at ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE careers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255),
+  career_type VARCHAR(255),
+  created_at DEFAULT CURRENT_TIMESTAMP,
+  updated_at ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE jobs (
+  id SERIAL PRIMARY KEY NOT NULL,
+  career_id INTEGER REFERENCES careers(id) ON DELETE CASCADE,
+  title VARCHAR(255),
+  jobs_blurb VARCHAR(255),
+  career_type VARCHAR(255),
+  created_at DEFAULT CURRENT_TIMESTAMP,
+  updated_at ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE quiz_results (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  recommentdation 1 VARCHAR(255),
+  recommentdation 2 VARCHAR(255),
+  recommentdation 3 VARCHAR(255),
+  created_at DEFAULT CURRENT_TIMESTAMP,
+  updated_at ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE questions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  questions VARCHAR(255),
+  option_a VARCHAR(255),
+  option_b VARCHAR(255),
+  option_c VARCHAR(255),
+  created_at DEFAULT CURRENT_TIMESTAMP,
+  updated_at ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE articles (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255),
+  job_id INTEGER REFERENCES jobs(id) ON DELETE CASCADE,
+  body_hyperlink,
+  created_at DEFAULT CURRENT_TIMESTAMP,
+  updated_at ON UPDATE CURRENT_TIMESTAMP
+);
