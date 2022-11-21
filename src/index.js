22fa2c378014cbedd40d8e594f5adea23d82dfd7
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-11-21 17:06:26 -0500

    create database

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const server = express()
+const fs = require("fs");
 const port = 8001
+const express = require('express')
+
+const server = express();
+const bodyparser = require("body-parser");
+// const db = require("./db");
 
 server.get('/', (req, res) => {
   res.send('Hello World!')
