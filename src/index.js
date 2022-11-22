b52866706aaef7c37ec7ea333772d39f24679b54
JCPenne <jackcpalmer@icloud.com>
2022-11-22 11:25:32 -0800

    add test route, add test-db route

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ const express = require('express');
 const server = express();
 const bodyparser = require('body-parser');
 const db = require('./db');
-//Set up a route for a get request to /test as mentioned in our front-end codebase
+
 server.get('/test', (req, res) => {
-  //Run a basic query to our database, requesting all rows from the 'tests' table
+  res.status(200).send('Test message: Back-end is ok');
+});
+server.get('/db-test', (req, res) => {
   db.query(
     `
     SELECT * from tests
     `,
-    //Once the query is complete send it to the client side (haven't quite figured out how formatting works yet)
   ).then(data => {
     res.send(data.rows[0]);
   });
