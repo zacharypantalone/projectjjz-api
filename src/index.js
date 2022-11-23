75ae5f4b5fa96e140573014a38440dcaa221162b
JCPenne <jackcpalmer@icloud.com>
2022-11-22 18:28:22 -0800

    create dummy query for post to register to display data for checking client side

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const port = 8001;
 const express = require('express');
 
 const server = express();
-const bodyparser = require('body-parser');
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
 const db = require('./db');
 
 server.get('/test', (req, res) => {
@@ -16,6 +18,22 @@ server.get('/db', (req, res) => {
   ).then(data => {
     res.send(data.rows[0]);
   });
+});
+
+server.post('/register', (req, res) => {
+  const firstName = req.body.firstname;
+  db.query(`SELECT first_name, last_name FROM users WHERE first_name=$1`, [
+    firstName,
+  ]).then(data => res.send(data.rows));
+
+  // res.send('Post to register received');
+  // db.query(
+  //   `
+  //   INSERT INTO users (first_name, last_name, email, password, password_confirmation)
+  //   VALUES
+  //   ($1,$2,$3,$4,$5)
+  //   `,[]
+  // )
 });
 
 server.listen(port, () => {
