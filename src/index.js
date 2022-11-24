574c9a58aaee9f181944b07532a23c6040eb6db8
JCPenne <jackcpalmer@icloud.com>
2022-11-23 17:27:50 -0800

    remove basic auth dependency and install nodemon

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 const port = 8001;
 const express = require('express');
-const basicAuth = require('express-basic-auth');
 
 const server = express();
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-// server.use(basicAuth({ authorizer: myAuthorizer }));
 
 const db = require('./db');
 
-// function myAuthorizer(email, password) {
-//   const userMatches = basicAuth.safeCompare(email, 'bob@ross.com');
-//   const passwordMatches = basicAuth.safeCompare(password, 'clouds');
-
-//   return userMatches & passwordMatches;
-// }
-
 server.post('/dashboard', (req, res) => {
-  console.log(req.body);
   const { email, password } = req.body.user;
   db.query(
     `
@@ -28,11 +18,6 @@ server.post('/dashboard', (req, res) => {
     [email, password],
   ).then(result => {
     console.log(result.rows);
-    // if (myAuthorizer(email, password)) {
-    //   res.status(200).send('User Login Authentication Successful');
-    // } else {
-    //   res.status(404).send('User not found');
-    // }
   });
 });
 
