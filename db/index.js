b2955bba3f2ec8de46abc5d3b31ecdfcc57688e8
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-11-24 21:03:32 -0500

    testing 123

--- db/index.js
+++
@@ -1,7 +0,0 @@
-//Create a connection to a our database named careersquared (see src/create.sql line 11)
-const { Client } = require('pg');
-const client = new Client({ connectionString: 'careersquared' });
-
-client.connect();
-
-module.exports = client;
--- src/db/schema/create.sql
+++ src/db/schema/create.sql
@@ -91,3 +91,5 @@ CREATE TABLE days (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL
 );
+
+-- testing 123
