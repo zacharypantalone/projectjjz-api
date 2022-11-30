6532e15b6459f999028a62c804c8cb96ec129252
JCPenne <jackcpalmer@icloud.com>
2022-11-30 13:26:02 -0800

    clean up seed.sql file

--- src/db/seeds/seed.sql
+++ src/db/seeds/seed.sql
@@ -1,56 +1,22 @@
 --  ///////////////////////////////////////////
 --  /////////       Users     /////////////////
 --  ///////////////////////////////////////////
--- INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES (info here);
+
 INSERT INTO users (first_name, last_name, email, password, password_confirmation) VALUES ('Bob','Ross','bob@ross.com','clouds','clouds');
-
-
---  ///////////////////////////////////////////
---  /////////       User_quiz     /////////////// JOINING TABLE BETWEEM USERS AND QUIZ
---  ///////////////////////////////////////////
-
--- INSERT INTO user_quiz (user_id, quiz_taken) VALUES (1, false);
-
-
-
---  ///////////////////////////////////////////
---  /////////       mentors     ///////////////
---  ///////////////////////////////////////////
-
-
--- INSERT INTO mentors (job_id, first_name, last_name, available_times, booked_times, created_at, updated_at) VALUES (info here);
--- INSERT INTO mentors (job_id, first_name, last_name, available_times, booked_times, created_at, updated_at) VALUES (1, 'Suzie', 'Summers',  FIGURE OUT DATES, FIGURE OUT BOOKED TIMES);
--- INSERT INTO mentors (job_id, first_name, last_name, available_times, booked_times, created_at, updated_at) VALUES (2, 'Paul', 'Parsons',  FIGURE OUT DATES, FIGURE OUT BOOKED TIMES);
--- INSERT INTO mentors (job_id, first_name, last_name, available_times, booked_times, created_at, updated_at) VALUES (3, 'Ned', 'Nielson',  FIGURE OUT DATES, FIGURE OUT BOOKED TIMES);
-
-
---  ///////////////////////////////////////////
---  /////////       appointments     //////////
---  ///////////////////////////////////////////
-
--- INSERT INTO appointments (user_id, mentor_id, appointment_time, created_at, updated_at) VALUES (info here);
--- INSERT INTO appointments (user_id, mentor_id, appointment_time, created_at, updated_at) VALUES (info here)
--- INSERT INTO appointments (user_id, mentor_id, appointment_time, created_at, updated_at) VALUES (info here)
--- INSERT INTO appointments (user_id, mentor_id, appointment_time, created_at, updated_at) VALUES (info here)
-
 
 --  ///////////////////////////////////////////
 --  /////////       careers     ///////////////
 --  ///////////////////////////////////////////
 
--- INSERT INTO careers (title, career_type, created_at, updated_at) VALUES (info here);
 INSERT INTO careers (career_type) VALUES ('Technology');
-
-
 
 --  ///////////////////////////////////////////
 --  /////////       jobs        ///////////////
 --  ///////////////////////////////////////////
 
--- INSERT INTO jobs (career_id, img, title, body, average_salary, salary_range, training, skills, learning_links, articles) VALUES (info here);
-
 INSERT INTO jobs (career_id, img, title, body, average_salary, salary_range, training, skills) VALUES (1,'https://cdn.dribbble.com/users/1626229/screenshots/16311321/media/0d2090c377191f4e49bde0932d71f9d3.jpg?compress=1&resize=400x300','Creative Coding','Like any form of expression, creative coding can be used to enhance your existing career or to start a new one. Experienced web developers might learn creative coding to help them create better interactive websites. Others use creative coding as a form of artistic expression. Agencies and start-ups are using creative coding to build interactive installations for the public and big brands',
 '$82,094/year', '$25,000 - $173,000/year', '6 months - 2 years', 'Javascript, P5.js, Sketch.js, Three.js, 3D');
+
 -- INSERT INTO jobs (career_id, img, title, body, average_salary, salary_range, training, skills, learning_links, articles) VALUES (1,'https://static.vecteezy.com/system/resources/thumbnails/008/516/079/small_2x/man-and-woman-are-working-together-on-programming-vector.jpg','Front End Developer','What is a front end developer youre wondering?! Their main task is the make sure websites are interacted with easily by users. Through a combination of coding and visual design, they are tasked with the front end building and upkeep of apps and websites.',
 -- '$72,208/year', '$49,000 - $106,000/year', '3 months - 2 years', 'Javascript, HTML, CSS, jQuery',  'https://www.freecodecamp.org/, https://www.udemy.com/course/crashing-course-for-web-front-end-development, https://www.thinkful.com/bootcamp/web-development/#introduction, https://www.lighthouselabs.ca/', 
 -- 'https://www.simplilearn.com/how-to-become-a-front-end-developer-article,  https://en.wikipedia.org/wiki/Front-end_web_development, https://www.weareadam.com/blog/2020/11/a-day-in-the-life-of-a-front-end-developer?source=google.com');
@@ -61,19 +27,10 @@ INSERT INTO jobs (career_id, img, title, body, average_salary, salary_range, tra
 --  ///////////////////////////////////////////
 --  /////////      learning_links      ////////
 --  ///////////////////////////////////////////
+
 INSERT INTO learning_links (link, jobs_id) VALUES ('https://thecodingtrain.com/tracks, https://www.kadenze.com/courses/the-nature-of-code-ii/info', 1);
 INSERT INTO learning_links (link, jobs_id) VALUES ('https://www.udemy.com/course/code-art/', 1);
 INSERT INTO learning_links (link, jobs_id) VALUES ('https://www.ecuad.ca/academics/teen-programs/summer-institute-for-teens/creative-coding-web-design-sit', 1);
-
---  ///////////////////////////////////////////
---  /////////      articles            ////////
---  ///////////////////////////////////////////
-
-INSERT INTO articles (article, jobs_id) VALUES ('https://interestingengineering.com/culture/everything-you-need-to-know-about-the-artistic-world-of-creative-coding', 1);
-INSERT INTO articles (article, jobs_id) VALUES ('https://www.udemy.com/course/code-art/', 1);
--- INSERT INTO articles (article, jobs_id) VALUES ('', 2);
--- INSERT INTO articles (article, jobs_id) VALUES ('', 3);
-
 
 --  ///////////////////////////////////////////
 --  /////////      questions           ////////
@@ -89,13 +46,27 @@ INSERT INTO questions (question, answer_one, answer_two) VALUES ('In your day-to
 --  /////////      quiz results    //////////// 
 --  ///////////////////////////////////////////
 
-
 INSERT INTO quiz_results (user_id, job_one_id, job_two_id, job_three_id) VALUES (1, 1, NULL, NULL);
 
 
 
---Test seed for the 'tests' table
--- INSERT INTO tests (body) VALUES ('This is the second test records body');
+--  ///////////////////////////////////////////
+--  /////////       mentors     ///////////////
+--  ///////////////////////////////////////////
+
+-- INSERT INTO mentors (job_id, first_name, last_name, available_times, booked_times, created_at, updated_at) VALUES (info here);
+-- INSERT INTO mentors (job_id, first_name, last_name, available_times, booked_times, created_at, updated_at) VALUES (1, 'Suzie', 'Summers',  FIGURE OUT DATES, FIGURE OUT BOOKED TIMES);
+-- INSERT INTO mentors (job_id, first_name, last_name, available_times, booked_times, created_at, updated_at) VALUES (2, 'Paul', 'Parsons',  FIGURE OUT DATES, FIGURE OUT BOOKED TIMES);
+-- INSERT INTO mentors (job_id, first_name, last_name, available_times, booked_times, created_at, updated_at) VALUES (3, 'Ned', 'Nielson',  FIGURE OUT DATES, FIGURE OUT BOOKED TIMES);
+
+--  ///////////////////////////////////////////
+--  /////////       appointments     //////////
+--  ///////////////////////////////////////////
+
+-- INSERT INTO appointments (user_id, mentor_id, appointment_time, created_at, updated_at) VALUES (info here);
+-- INSERT INTO appointments (user_id, mentor_id, appointment_time, created_at, updated_at) VALUES (info here)
+-- INSERT INTO appointments (user_id, mentor_id, appointment_time, created_at, updated_at) VALUES (info here)
+-- INSERT INTO appointments (user_id, mentor_id, appointment_time, created_at, updated_at) VALUES (info here)
 
 
 
